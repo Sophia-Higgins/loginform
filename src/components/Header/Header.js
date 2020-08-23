894ce8d2edd8921e3548910e11e95d887d2826d4
Sophia <69153714+Sophia-Higgins@users.noreply.github.com>
2020-08-23 15:29:44 +0100

    Add Logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { withRouter } from "react-router-dom";
+import { ACCESS_TOKEN_NAME } from '../../constants/apiConstants';
 
-function Header() {
+function Header(props) {
+
+  function renderLogout() {
+    if (props.location.pathname === '/home') {
+      return (
+        <div className="ml-auto">
+          <button className="btn btn-light" onClick={() => handleLogout()}>Logout</button>
+        </div>
+      )
+    }
+  }
+
+  function handleLogout() {
+    localStorage.removeItem(ACCESS_TOKEN_NAME);
+    props.history.push('/login');
+  }
+
   return(
     <nav className="navbar navbar-dark bg-primary">
       <div className="row col-12 d-flex justify-content-center text-white">
         <span className="h3">The Good Place</span>
+        {renderLogout()}
       </div>
     </nav>
   )
 }
-export default Header;
+export default withRouter(Header);
