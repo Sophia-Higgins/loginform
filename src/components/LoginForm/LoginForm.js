722cafdd07409d278cfa598d975913921ce4e087
Sophia <69153714+Sophia-Higgins@users.noreply.github.com>
2020-08-23 14:48:21 +0100

    Fix error handling

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,77 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_BASE_URL,  ACCESS_TOKEN_NAME } from '../../constants/apiConstants';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL, ACCESS_TOKEN_NAME } from "../../constants/apiConstants";
 import { withRouter } from "react-router-dom";
 
 function LoginForm(props) {
-  useEffect(() => {document.body.style.backgroundColor = '#fff'}, []);
-  
+  useEffect(() => {
+    document.body.style.backgroundColor = "#fff";
+  }, []);
+
   const [state, setState] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
     errorMessage: null,
-  })
+  });
 
   const handleChange = (e) => {
-    const {id, value} = e.target
-    setState(prevState => ({
+    const { id, value } = e.target;
+    setState((prevState) => ({
       ...prevState,
-      [id]: value
-    }))
-  }
+      [id]: value,
+    }));
+  };
 
   const handleSubmitClick = (e) => {
     e.preventDefault();
-    const payload={
-      'username': state.username,
-      'password': state.password,
-    }
-    axios.post(API_BASE_URL+'/login', payload)
+    const payload = {
+      username: state.username,
+      password: state.password,
+    };
+    axios
+      .post(API_BASE_URL + "/login", payload)
       .then((response) => {
         if (response.status === 200) {
           localStorage.setItem(ACCESS_TOKEN_NAME, response.data.token);
           redirectToHome();
-          props.showError(null)
         }
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((error) => {
+        if (error.response.status === 401) {
+          setState((prevState) => ({
+            ...prevState,
+            errorMessage: "Invalid Username or Password. Please try again.",
+          }));
+        } else {
+          setState((prevState) => ({
+            ...prevState,
+            errorMessage:
+              "Something went wrong...Are you sure you belong in The Good Place?",
+          }));
+        }
       });
-  }
+  };
 
   const redirectToHome = () => {
-    props.history.push('/home');
-  }
+    props.history.push("/home");
+  };
 
   return (
-    <div className='card w-50 login-card mt-5 hv-center'>
-      <div className='card-header bg-primary text-white'>Please Login</div>
+    <div className="card w-50 login-card mt-5 hv-center">
+      <div className="card-header bg-primary text-white">Please Login</div>
       <form>
-      <br />
-        <div className='form-group col-lg-12 text-left'>
-          <label htmlFor='exampleUsername'>Username</label>
-          <input type='username'
-            className='form-control'
-            id='username'
-            placeholder='Username'
+        <br />
+        <div className="form-group col-lg-12 text-left">
+          <label htmlFor="exampleUsername">Username</label>
+          <input
+            type="username"
+            className="form-control"
+            id="username"
+            placeholder="Username"
             value={state.username}
             onChange={handleChange}
           />
         </div>
-        <div className='form-group col-lg-12 text-left'>
-          <label htmlFor='inputPassword'>Password</label>
-          <input type='password'
-            className='form-control'
-            id='password'
-            placeholder='Password'
+        <div className="form-group col-lg-12 text-left">
+          <label htmlFor="inputPassword">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
             value={state.password}
             onChange={handleChange}
           />
           <br />
-          <div className='row justify-content-center'>
+          <div
+            className="alert alert-danger mt-2"
+            style={{ display: state.errorMessage ? "block" : "none" }}
+            role="alert"
+          >
+            {state.errorMessage}
+          </div>
+          <div className="row justify-content-center">
             <button
-              type='submit'
-              className='btn btn-primary'
+              type="submit"
+              className="btn btn-primary"
               onClick={handleSubmitClick}
             >
               Login
@@ -80,7 +102,7 @@ function LoginForm(props) {
         </div>
       </form>
     </div>
-  )
+  );
 }
 
 export default withRouter(LoginForm);
